117ac8e5a363e07d4a2200b970379bd41f5bd244
Laurent <lawz@free.fr>
2019-01-25 18:21:14 +0100

    First Commmit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// inView('.rvl')
+//     .on('enter', function(this){
+//         console.log(this);
+//         this.classList.add("mystyle")
+//     });
+
+    inView('.rvl')
+    .on('enter', function (el) {
+        el.classList.add("inview");
+    })
+    .on('exit', function (el) {
+        el.classList.remove("inview");
+    });
