e4e5c447542ca3fa5e47479fe00578506ecec0a1
Laurent <lawz@free.fr>
2019-01-29 00:35:41 +0100

    seventh  Commmit

--- js/main.js
+++ js/main.js
@@ -4,12 +4,15 @@
 //         this.classList.add("mystyle")
 //     });
 
+    inView.offset(-100);
     inView('.rvl')
     .on('enter', function (el) {
         el.classList.add("inview");
+        console.log("inview");
     })
     // .on('exit', function (el) {
     //     el.classList.remove("inview");
+    //     console.log("offview");
     // });
 
 
